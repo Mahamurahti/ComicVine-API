8a5e9ce22853264468ee80b96a7d99cc90b1802b
Mahamurahti <dagility1@gmail.com>
2020-04-28 01:07:08 +0300

    Added some style, fetching now works

--- js/fetchScript.js
+++ js/fetchScript.js
@@ -6,14 +6,28 @@ let section = document.getElementById('outputSection');
 let input = document.getElementById('inputField');
 
 searchButton.addEventListener('click', function(evt) {
-  inputValue = document.getElementById('inputfield').value;
-  console.log(inputValue);
+  inputValue = document.getElementById('inputField').value;
   fetchComics(inputValue);
 });
 
+searchButton.click(function() {
+  inputValue = document.getElementById('inputfield').value;
+  fetchComics(inputValue);
+});
+
+input.addEventListener('keyup', function(evt) {
+  evt.preventDefault();
+  if (evt.keyCode === 13) {
+    searchButton.click();
+  }
+});
+
 // -----------------------------FETCHING------------------------------------//
+let proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+    targetUrl = `https://comicvine.gamespot.com/api/`;
+
 function fetchComics(data) {
-  fetch(``).
+  fetch(proxyUrl + targetUrl + `volumes/?api_key=ee41526160ae9968da980fa835fe8f869cfce78f&format=json&sort=name:asc&filter=name:${data}`).
       then(function(response) {
         return response.json();
       }).
@@ -27,7 +41,15 @@ function fetchComics(data) {
       });
 }
 
-function printComicInformation() {
+function printComicInformation(data) {
+
   section.innerHTML = "";
+
+  for (let i = 0; i <= data.results.length; i++) {
+    section.innerHTML += `<section class="comicInfo">
+          <img src="${data.results[i].image.medium_url}" alt="${data.results[i].name}">
+          <h1>${data.results[i].name}</h1>
+          </section>`;
+  }
 
 }
